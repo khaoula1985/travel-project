f44107440f37c158d0ddd4dc93b5e1e18f88edbc
khaoula1985 <khaoula.kaddachi@esprit.tn>
2023-12-05 01:01:36 +0100

    first commit

--- front/src/pages/Gallery.js
+++ front/src/pages/Gallery.js
@@ -5,26 +5,28 @@ import './Gallery.css';
 import avions from '../images/travelnotes.png';
 import Masonry from 'react-layout-masonry';
 import { RiDeleteBin5Fill } from 'react-icons/ri';
-import { FaRegCommentDots } from "react-icons/fa";
-import { FaPen } from "react-icons/fa";
+import { FaRegCommentDots, FaPen } from 'react-icons/fa';
 
 const Gallery = () => {
   const [file, setFile] = useState(null);
   const [comment, setComment] = useState('');
   const [galleryItems, setGalleryItems] = useState([]);
-  const [username, setUsername] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
   const [likes, setLikes] = useState(() => {
     const storedLikes = localStorage.getItem('likes');
     return storedLikes ? JSON.parse(storedLikes) : {};
   });
-
-  const [question, setQuestion] = useState('');
+  const [dislikes, setDislikes] = useState(() => {
+    const storedDislikes = localStorage.getItem('dislikes');
+    return storedDislikes ? JSON.parse(storedDislikes) : {};
+  });
   const [showQuestionInput, setShowQuestionInput] = useState(false);
-  const [questions, setQuestions] = useState({});
+  const [comments, setComments] = useState(() => {
+    const storedComments = localStorage.getItem('comments');
+    return storedComments ? JSON.parse(storedComments) : {};
+  });
   const [commentInResponse, setCommentInResponse] = useState('');
-
   const [selectedItemId, setSelectedItemId] = useState(null);
 
   const handleImageChange = (e) => {
@@ -111,22 +113,33 @@ const Gallery = () => {
     setShowQuestionInput(true);
   };
 
-  const handlePostCommentClick = () => {
+  const handlePostCommentClick = async () => {
     if (selectedItemId) {
-      const updatedQuestions = { ...questions };
-      updatedQuestions[selectedItemId] = [
-        ...(updatedQuestions[selectedItemId] || []),
-        commentInResponse,
-      ];
-      setQuestions(updatedQuestions);
-      setShowQuestionInput(false);
-      setCommentInResponse('');
-      setSelectedItemId(null);
-    }
-  };
+      try {
+        const token = localStorage.getItem('token');
+        const decoded = jwt_decode(token);
+        const userId = decoded.userId;
 
-  const handleQuestionChange = (e) => {
-    setQuestion(e.target.value);
+        const response = await axios.get(`http://localhost:8080/user/${userId}`);
+        const user = response.data;
+        const username = user.username;
+
+        const updatedComments = { ...comments };
+        updatedComments[selectedItemId] = [
+          ...(updatedComments[selectedItemId] || []),
+          `${username}: ${commentInResponse}`,
+        ];
+
+        setComments(updatedComments);
+        setShowQuestionInput(false);
+        setCommentInResponse('');
+        setSelectedItemId(null);
+
+        localStorage.setItem('comments', JSON.stringify(updatedComments));
+      } catch (error) {
+        console.error('Error retrieving user information:', error);
+      }
+    }
   };
 
   const handleCommentInResponseChange = (e) => {
@@ -142,8 +155,22 @@ const Gallery = () => {
   useEffect(() => {
     return () => {
       localStorage.setItem('likes', JSON.stringify(likes));
+      localStorage.setItem('dislikes', JSON.stringify(dislikes));
+      localStorage.setItem('comments', JSON.stringify(comments));
     };
-  }, [likes]);
+  }, [likes, dislikes, comments]);
+
+  const handledisLikeClick = (itemId) => {
+    if (isLoggedIn) {
+      const updatedDislikes = { ...dislikes };
+      updatedDislikes[itemId] = (updatedDislikes[itemId] || 0) + 1;
+      setDislikes(updatedDislikes);
+
+      localStorage.setItem('dislikes', JSON.stringify(updatedDislikes));
+    } else {
+      alert('Log in to dislike photos');
+    }
+  };
 
   return (
     <>
@@ -151,12 +178,12 @@ const Gallery = () => {
         <h1>Share your travel experience with us!</h1>
         {isLoggedIn ? (
           <div className='memory'>
-            <img src={avions} alt="memory" />
+            <img src={avions} alt='memory' />
             <div>
               <h3>Add a Photo</h3>
-              <input type="file" onChange={handleImageChange} className='inputfile' />
+              <input type='file' onChange={handleImageChange} className='inputfile' />
               <h3>Add a comment</h3>
-              <input type="text" value={comment} onChange={handleCommentChange} />
+              <input type='text' value={comment} onChange={handleCommentChange} />
               <button onClick={handleClick}>Upload</button>
             </div>
           </div>
@@ -172,40 +199,61 @@ const Gallery = () => {
       >
         {galleryItems.map((item, index) => (
           <div className='Gitem' key={index}>
-            <img src={`http://localhost:8080/uploads/` + item.image} alt="img" />
-            <p><span>Comment:</span>{item.comment}</p>
-            <p><span>Uploaded by:</span> {item.username}</p>
-            <p><span>Date:</span> {new Date(item.createdAt).toDateString()}</p>
-            <div className="actions-container">
+            <img src={`http://localhost:8080/uploads/` + item.image} alt='img' />
+            <p>
+              <span>Comment:</span>
+              {item.comment}
+            </p>
+            <p>
+              <span>Uploaded by:</span> {item.username}
+            </p>
+            <p>
+              <span>Date:</span> {new Date(item.createdAt).toDateString()}
+            </p>
+            <div className='actions-container'>
               <div className='iconss'>
-              <div className="likes-container">
-                <button onClick={() => handleLikeClick(item._id)}>
-                  ❤️
+                <div className='likes-container'>
+                  <button onClick={() => handleLikeClick(item._id)}>❤️</button>
+                  <span>{likes[item._id] || 0}</span>
+                </div>
+
+                <div className='dislikes-container'>
+                  <button onClick={() => handledisLikeClick(item._id)}>😕</button>
+                  <span>{dislikes[item._id] || 0}</span>
+                </div>
+
+                <button onClick={() => handleCommentClick(item._id)}>
+                  <FaRegCommentDots />
                 </button>
-                <span>{likes[item._id] || 0}</span>
+                {isAdmin ? (
+                  <button onClick={() => handleDelete(item._id)} className='del'>
+                    <RiDeleteBin5Fill />
+                  </button>
+                ) : (
+                  <button disabled={true} className='del'>
+                    <RiDeleteBin5Fill />
+                  </button>
+                )}
               </div>
-              <button onClick={() => handleCommentClick(item._id)}><FaRegCommentDots/> </button>
-              {isAdmin ? (
-                <button onClick={() => handleDelete(item._id) }className="del"><RiDeleteBin5Fill /></button>
-              ) : (
-                <button disabled={true} className="del"><RiDeleteBin5Fill /></button>
-              )}
-            </div>
             </div>
             {showQuestionInput && selectedItemId === item._id && (
-              <div className="question-input-container">
-                <input type="text" placeholder="Add a comment" value={commentInResponse} onChange={handleCommentInResponseChange} />
-                <button onClick={handlePostCommentClick}><FaPen/></button>
+              <div className='question-input-container'>
+                <input
+                  type='text'
+                  placeholder='Add a comment'
+                  value={commentInResponse}
+                  onChange={handleCommentInResponseChange}
+                />
+                <button onClick={handlePostCommentClick}>
+                  <FaPen />
+                </button>
               </div>
             )}
-            {questions[item._id] && questions[item._id].length > 0 && (
-              <div className="questions-container">
-               
+            {comments[item._id] && comments[item._id].length > 0 && (
+              <div className='questions-container'>
                 <ul>
-                  {questions[item._id].map((q, index) => (
-                    <li key={index}>{q}
-                    
-                    </li>
+                  {comments[item._id].map((q, index) => (
+                    <li key={index}>{q}</li>
                   ))}
                 </ul>
               </div>
